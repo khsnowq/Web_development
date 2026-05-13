7d41442e6bc114244cc1a42fc23085d8675dd780
karina-kbtu <lastochka273@gmail.com>
2026-05-14 02:41:59 +0500

    Initial commit

--- project_diary/diary_front/src/app/models/mood-entry.model.ts
+++
@@ -1,7 +0,0 @@
-export interface MoodEntry {
-    id?: number;
-    date?: string;
-    mood: string;
-    note: string;
-  }
-  
--- project_diary/diary_front/src/app/models/wishlist.model.ts
+++
@@ -1,8 +0,0 @@
-export interface WishlistItem {
-  id?: number;
-  name: string;  // было title, теперь name
-  category: 'material' | 'travel' | 'learning';  // исправлено на корректные категории
-  status: 'pending' | 'in_progress' | 'done';  // исправлено на корректные статусы
-  image?: string;
-  link?: string;
-}
